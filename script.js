379ed1dc5473bcd87bd86582f6575d9df6a3c13b
KoriH <96402323+KoriH@users.noreply.github.com>
2024-05-15 21:44:28 -0700

    add book modal template

--- script.js
+++ script.js
@@ -31,38 +31,46 @@ const openAddBookModal = () => {
 }
 
 
-const addButton = document.getElementById("add-book").addEventListener('click', function () {
-    let book = document.createElement('div');
-    book.className = 'book';
-    let title = document.createElement('p');
-    title.className = 'book-text';
-    title.textContent = 'Title';
-    let author = document.createElement('p');
-    author.className = 'book-text';
-    author.textContent = 'Author';
-    let pages = document.createElement('p');
-    pages.className = 'book-text';
-    pages.textContent = 'Pages';
 
-    let readButton = document.createElement('button');
-    readButton.className = 'button-read';
-    let readButtonText = document.createElement('p');
-    readButtonText.className = 'book-text';
-    readButtonText.textContent = 'Read';
-    readButton.appendChild(readButtonText);
 
-    let removeButton = document.createElement('button');
-    removeButton.className = 'remove';
-    let removeButtonText = document.createElement('p');
-    removeButtonText.className = 'book-text';
-    removeButtonText.textContent = 'Remove';
-    removeButton.appendChild(removeButtonText);
+const addButton = document.getElementById("add-book")
+addButton.addEventListener('click', function () {
 
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(readButton);
-    book.appendChild(removeButton);
+    const addBookModal = document.getElementById("add-book-form");
+    addBookModal.classList.remove("hidden");
 
-    bookGrid.appendChild(book);
+
+    // let book = document.createElement('div');
+    // book.className = 'book';
+    // let title = document.createElement('p');
+    // title.className = 'book-text';
+    // title.textContent = 'Title';
+    // let author = document.createElement('p');
+    // author.className = 'book-text';
+    // author.textContent = 'Author';
+    // let pages = document.createElement('p');
+    // pages.className = 'book-text';
+    // pages.textContent = 'Pages';
+
+    // let readButton = document.createElement('button');
+    // readButton.className = 'button-read';
+    // let readButtonText = document.createElement('p');
+    // readButtonText.className = 'book-text';
+    // readButtonText.textContent = 'Read';
+    // readButton.appendChild(readButtonText);
+
+    // let removeButton = document.createElement('button');
+    // removeButton.className = 'remove';
+    // let removeButtonText = document.createElement('p');
+    // removeButtonText.className = 'book-text';
+    // removeButtonText.textContent = 'Remove';
+    // removeButton.appendChild(removeButtonText);
+
+    // book.appendChild(title);
+    // book.appendChild(author);
+    // book.appendChild(pages);
+    // book.appendChild(readButton);
+    // book.appendChild(removeButton);
+
+    // bookGrid.appendChild(book);
 })
